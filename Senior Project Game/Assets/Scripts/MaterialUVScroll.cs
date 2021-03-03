a7e4eebfb38319ee87abc36af07013611fa956e3
PhantomDonut <phantomdonut@yahoo.com>
2021-03-02 22:46:10 -0800

    Dark Blocks & Wall Jump Animations
    Committed a few weeks after working, preparing to import new shader package.

---
+++ Senior Project Game/Assets/Scripts/MaterialUVScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialUVScroll : MonoBehaviour {
+
+    [SerializeField] private Vector2 scrollSpeed;
+    private Material material;
+    private Vector2 offset;
+
+    private void Start() {
+        material = GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    private void Update() {
+        offset.x += Time.deltaTime * scrollSpeed.x * 0.001f;
+        offset.y += Time.deltaTime * scrollSpeed.y * 0.001f;
+        material.mainTextureOffset = offset;
+    }
+}
